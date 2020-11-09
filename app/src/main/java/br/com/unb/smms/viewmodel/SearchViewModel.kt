7b4b4847dcad83d4a55ea741e83e64117092c0e5
Victoria  Goularte <victoriagoularte@MacBook-Air-de-Victoria.local>
2020-11-09 20:58:38 -0300

    improving experience in search

--- app/src/main/java/br/com/unb/smms/viewmodel/SearchViewModel.kt
+++ app/src/main/java/br/com/unb/smms/viewmodel/SearchViewModel.kt
@@ -1,12 +1,12 @@
 package br.com.unb.smms.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.unb.smms.SmmsData
 import br.com.unb.smms.domain.firebase.Post
 import br.com.unb.smms.interactor.FirebaseInteractor
-import br.com.unb.smms.interactor.PageInteractor
 import io.reactivex.disposables.Disposable
 
 class SearchViewModel @ViewModelInject constructor(
@@ -17,15 +17,26 @@ class SearchViewModel @ViewModelInject constructor(
     var post = MutableLiveData<String>()
     val annotation = MutableLiveData<String>()
 
+    private val _showList = MutableLiveData<Boolean>()
+    val showList: LiveData<Boolean>
+        get() = _showList
+
     var resultSearch = MutableLiveData<SmmsData<List<Post>>>()
 
     private lateinit var disposable: Disposable
 
     fun search() {
-        disposable = firebaseInteractor.search(title.value, post.value, annotation.value).subscribe { res, error ->
-            if(error != null) {
-                resultSearch.value = SmmsData.Error(error)
-                return@subscribe
+
+        _showList.value = true
+
+        disposable = firebaseInteractor.search(title.value, post.value, annotation.value)
+            .subscribe { res, error ->
+
+                _showList.value = false
+
+                if (error != null) {
+                    resultSearch.value = SmmsData.Error(error)
+                    return@subscribe
             }
 
             resultSearch.value = SmmsData.Success(res.toList())
