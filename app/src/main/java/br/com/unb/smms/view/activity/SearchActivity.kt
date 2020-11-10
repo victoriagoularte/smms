6403bb6d75ca6e85bf123d4bf57488f2f1eaf4d1
Victoria  Goularte <victoriagoularte@MacBook-Air-de-Victoria.local>
2020-11-09 21:14:17 -0300

    fixing back button in search fragment

--- app/src/main/java/br/com/unb/smms/view/activity/SearchActivity.kt
+++ app/src/main/java/br/com/unb/smms/view/activity/SearchActivity.kt
@@ -2,13 +2,24 @@ package br.com.unb.smms.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
 import br.com.unb.smms.R
+import br.com.unb.smms.databinding.ActivitySearchBinding
+import br.com.unb.smms.databinding.ActivitySmmsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.lifecycleOwner = this
     }
+
 }
